6e56f6eb39d1370576e34e887ed931159586b141
ayomide akinrata <akinrataa@gmail.com>
2026-03-04 05:55:46 +0100

    input the right url

--- src/pages/Getstartpage/Getstartpage.jsx
+++ src/pages/Getstartpage/Getstartpage.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 // API Service
 // const API_URL =  'https://sekanihubbackend.onrender.com';
-const API_URL = 'https://api.sekanihub.com';
+const API_URL = 'https://api.sekanihub.com/api';
 
 export default function StartYourProject() {
   const navigate = useNavigate();
